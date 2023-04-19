3ff2e79b24cc8138e992b79691ddf97f117c4346
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-19 17:50:30 +0200

    features-constructors-getters-and-setters-shop

--- CSharpShop/Shop.cs
+++ CSharpShop/Shop.cs
@@ -8,6 +8,36 @@ namespace Shop
 {
     public class shop
     {
+        // FEATURES
+        private string shopName;
+        private string address;        
+        private int houseNumber;
+        private string citty;
 
+        // CONSTRUCTORS
+
+        public shop(string shopName, string address, int houseNumber, string citty)
+        {
+            this.shopName = shopName;
+            this.address = address;        
+            this.houseNumber = houseNumber;
+            this.citty = citty;
+        }
+
+        // GETTERS
+
+        public string GetShopName { get { return shopName; } }
+        public string GetAddress { get { return address; } }
+        public int GetHouseNumber { get { return houseNumber; } }
+        public string GetCitty { get { return citty; } }
+
+        // SETTERS
+
+        public void SetShopName(string shopName) { this.shopName = shopName; }
+        public void SetAddress(string address) { this.address = address;}
+        public void SetHouseNumber(int houseNUmber) { this.houseNumber = houseNUmber;}
+        public void SetCitty(string citty) { this.citty = citty;}
+
+        
     }
 }
